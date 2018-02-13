6de36a14cffa3f1e48cc42ff5a27e30ec0bcdb96
NimitzDEV <nimitzdevteam@gmail.com>
2018-02-13 11:49:55 +0800

    no need to use useMongoClient options anymore

--- server/db/mongo/index.js
+++ server/db/mongo/index.js
@@ -4,7 +4,7 @@ const config = require('../../../config');
 module.exports = class DBConfig {
   static init() {
     mongoose.Promise = global.Promise;
-    mongoose.connect(config.mongo_addr, { useMongoClient: true }, err => {
+    mongoose.connect(config.mongo_addr, err => {
       if (err) console.error('ERROR CONNECTING MONGODB');
     });
   }
